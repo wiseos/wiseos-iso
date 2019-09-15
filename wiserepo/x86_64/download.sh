b0257b1fd223af5e2713ce5dd3a7c926c0fc85c7
metalneox <metalneox@gmail.com>
2019-09-15 20:23:56 +0200

    dep

--- wiserepo/x86_64/download.sh
+++ wiserepo/x86_64/download.sh
@@ -2,30 +2,30 @@
 function calamares {
 	git clone https://github.com/wiseos/wiseos-pkgbuild.git
 	cd wiseos-pkgbuild/calamares-git 
-	makepkg . && mv *.tar.xz ../.. && cd .. && cd .. && rm -rf && rm -rf wiseos-pkgbuild
+	makepkg -s . && mv *.tar.xz ../.. && cd .. && cd .. && rm -rf && rm -rf wiseos-pkgbuild
 }
 
 function download {
 	wget https://aur.archlinux.org/cgit/aur.git/snapshot/$1.tar.gz
 	tar -xvf $1.tar.gz && cd $1
-	makepkg . && mv *.tar.xz .. && cd .. && rm -rf $1 && rm $1.tar.gz
+	makepkg -s . && mv *.tar.xz .. && cd .. && rm -rf $1 && rm $1.tar.gz
 }
 
 #TODO need optimization with un loop from file
-#calamares
+calamares
 
-#download cava
-#download yay
-#download polybar
-#download shantz-xwinwrap-bzr
-#download ckbcomp
-#download betterlockscreen 
-#download mkinitcpio-openswap
-#download gotop
-#download curseradio-git
-#download ttf-weather-icons
-#download nerd-fonts-hack
-#download nerd-fonts-iosevka
+download cava
+download yay
+download polybar
+download shantz-xwinwrap-bzr
+download ckbcomp
+download betterlockscreen 
+download mkinitcpio-openswap
+download gotop
+download curseradio-git
+download ttf-weather-icons
+download nerd-fonts-hack
+download nerd-fonts-iosevka
 download wpa_actiond
 
 
