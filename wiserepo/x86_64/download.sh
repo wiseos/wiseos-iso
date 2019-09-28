4c8a41b97338ab40016ab4760138a3f43ca80aac
MetalNeox <metalneox@gmail.com>
2019-09-28 16:41:22 +0200

    calamares update

--- wiserepo/x86_64/download.sh
+++ wiserepo/x86_64/download.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 function calamares {
-	git clone https://github.com/wiseos/wiseos-pkgbuild.git
-	cd wiseos-pkgbuild/calamares-git 
+	git clone https://github.com/wiseos/calamares.git
+	cd calamares/pkg
 	makepkg -s . && mv *.tar.xz ../.. && cd .. && cd .. && rm -rf && rm -rf wiseos-pkgbuild
 }
 
 function download {
 	wget https://aur.archlinux.org/cgit/aur.git/snapshot/$1.tar.gz
 	tar -xvf $1.tar.gz && cd $1
+	#testing delete file
 	makepkg -s . && mv *.tar.xz .. && cd .. && rm -rf $1 && rm $1.tar.gz
 }
 
-#calamares
+calamares
 
 input="./aur"
 
